2f427f27c6c7e6ca5c5af3d441c720a1e3d590a2
Sechaba <sechtm@gmail.com>
2021-02-16 15:44:56 +0200

    wow

--- Lib/site-packages/notion/store.py
+++ Lib/site-packages/notion/store.py
@@ -289,7 +289,11 @@ class RecordStore(object):
 
     def store_recordmap(self, recordmap):
         for table, records in recordmap.items():
+            if not isinstance(records, dict):
+                continue
             for id, record in records.items():
+                if not isinstance(record, dict):
+                    continue
                 self._update_record(
                     table, id, value=record.get("value"), role=record.get("role")
                 )
@@ -301,18 +305,20 @@ class RecordStore(object):
         search="",
         type="table",
         aggregate=[],
-        filter=[],
-        filter_operator="and",
+        aggregations=[],
+        filter={},
         sort=[],
         calendar_by="",
         group_by="",
     ):
 
+        assert not (
+            aggregate and aggregations
+        ), "Use only one of `aggregate` or `aggregations` (old vs new format)"
+
         # convert singletons into lists if needed
         if isinstance(aggregate, dict):
             aggregate = [aggregate]
-        if isinstance(filter, dict):
-            filter = [filter]
         if isinstance(sort, dict):
             sort = [sort]
 
@@ -322,15 +328,15 @@ class RecordStore(object):
             "loader": {
                 "limit": 10000,
                 "loadContentCover": True,
-                "query": search,
+                "searchQuery": search,
                 "userLocale": "en",
                 "userTimeZone": str(get_localzone()),
                 "type": type,
             },
             "query": {
                 "aggregate": aggregate,
+                "aggregations": aggregations,
                 "filter": filter,
-                "filter_operator": filter_operator,
                 "sort": sort,
             },
         }
--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ import os
 from decouple import config
 
 # Store these values in an .env file in this directory (See "/.env.example").
-CLIPPINGS_FILE = config('CLIPPINGS_FILE')
-NOTION_TOKEN = config('NOTION_TOKEN')
-NOTION_TABLE_ID = config('NOTION_TABLE_ID')
+CLIPPINGS_FILE = config('CLIPPINGS_FILE', default="MyClippings.txt")
+NOTION_TOKEN = config(
+    'NOTION_TOKEN', default="5b247e27937dba1a03ca1b88833e05eed7b8ad84356d1cfdf7148797918e213e559d3af8a0c85a1d08726c10097a6d5181dcdec56fba78de1dc08f57be6947ee19d210a51ddcaa9536cb032c3583")
+NOTION_TABLE_ID = config(
+    'NOTION_TABLE_ID', default="https://www.notion.so/d61023cf4f134f12926bc34d785f097a?v=278b5c16962c4d6bbad0bbeeb7794054")
